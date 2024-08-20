9b11e775e51cd06045422d7007172b12a37c6c8a
devopsbaba <devopsbaba2011@gmail.com>
2024-08-20 14:31:39 +0530

    first commit

---
+++ src/main/java/TestJavaAutomation/Testjava/App.java
@@ -0,0 +1,13 @@
+package TestJavaAutomation.Testjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
